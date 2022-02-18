de1cf8e2975b2ac54d08fce7e714002dc64119f5
Dharmik Parmar <75156210+itsdharmik@users.noreply.github.com>
2022-02-18 09:32:19 +0000

    added weather

--- main.py
+++ main.py
@@ -20,4 +20,61 @@ try:
 except:
     print("Error during authentication")
 
-api.update_status("")
+
+# importing requests and json
+import requests, json
+# base URL
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
+CITY = "Ahmedabad"
+# API key API_KEY = "Your API Key"
+# upadting the URL
+URL = BASE_URL + "q=" + CITY + "&appid=" + "API KEY"
+# HTTP request
+response = requests.get(URL)
+# checking the status code of the request
+if response.status_code == 200:
+   # getting data in the json format
+   data = response.json()
+   # getting the main dict block
+   main = data['main']
+   # getting temperature
+   tempfetched = main['temp'] - 273
+
+  #Formatted temperature without decimal
+   temperature = '{0:.2g}'.format(tempfetched)
+  
+   # getting the humidity
+   humidity = main['humidity']
+   # getting the pressure
+   pressure = main['pressure']
+   # weather report
+   report = data['weather']
+   print(f"{CITY:-^30}")
+   print(f"Temperature: {temperature}")
+   print(f"Humidity: {humidity}")
+   print(f"Pressure: {pressure}")
+   print(f"Weather Report: {report[0]['description']}")
+else:
+   # showing the error message
+   print("Error in the HTTP request")
+
+
+
+
+import schedule
+import time
+  
+def func():
+  api.update_status(
+    (f"{CITY:-^30}""\n"
+     f"Temperature: {temperature}""\n"
+     f"Humidity: {humidity}""\n"
+     f"Pressure: {pressure}""\n"
+     f"Weather Report: {report[0]['description']}")
+)
+  
+schedule.every(60).minutes.do(func)
+  
+while True:
+    schedule.run_pending()
+    time.sleep(60)
